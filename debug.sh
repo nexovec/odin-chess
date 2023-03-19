c03961ebe6aeab0a577838ffd7bf1b59694d2104
nexovec <hraccesky@gmail.com>
2023-03-19 10:05:52 +0100

    platform specific workspaces

--- debug.sh
+++ debug.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo apt install libsdl2-image-dev libsdl2-ttf-dev
-~/src/Odin/odin run src -warnings-as-errors -debug -lld -o:minimal -show-timings -microarch:native -thread-count:6 -out:build/chesst.exe -collection:libs=libs
+~/src/Odin/odin build src -warnings-as-errors -debug -lld -o:minimal -show-timings -microarch:native -thread-count:6 -out:build/chesst.exe -collection:libs=libs
